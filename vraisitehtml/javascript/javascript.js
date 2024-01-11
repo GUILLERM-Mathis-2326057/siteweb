79d4b9ca74e3a2a965261327333896b95ce6bac2
GUILLERM-Mathis-23016147 <144850262+GUILLERM-Mathis-2326057@users.noreply.github.com>
2024-01-11 09:18:42 +0100

    Delete vraisitehtml directory

--- vraisitehtml/javascript/javascript.js
+++
@@ -1,6 +0,0 @@
-const modeButton = document.getElementById("mode-button");
-
-    modeButton.addEventListener("click", function() {
-      // Active le mode sombre
-      document.body.classList.toggle("mode-dark");
-    });
